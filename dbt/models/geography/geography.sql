f4f78a86d0fc39d1d777fe0c5c310e93d1fc028a
Giancarlo Masera <giancarlo@levity.ai>
2021-06-08 09:07:35 +0000

    commit

--- dbt/models/geography/geography.sql
+++ dbt/models/geography/geography.sql
@@ -1,5 +1,6 @@
+{{ config(materialized='table', sort='contact_country', dist='user_email_address') }}
 select 
-user_email_address
+user_email_address,
 contact_country
 from {{ref('production_users')}} pu
 inner join {{ref('intercom_contacts')}} ic
