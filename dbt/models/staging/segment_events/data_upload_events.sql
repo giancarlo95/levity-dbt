b779a2e47d482ad5e05ad6f07b490859e1fbd313
Juan Ugueto <juan@levity.ai>
2021-10-22 13:01:30 +0000

    Segment Events

--- dbt/models/staging/segment_events/data_upload_events.sql
+++ dbt/models/staging/segment_events/data_upload_events.sql
@@ -1,1 +1,38 @@
-trial
+WITH datasets_data AS (
+
+    SELECT 
+        user_id,
+        aiblock_id,
+        datapoint_id,
+        date_datapoint_uploaded,
+        account_id
+    FROM 
+        {{ref('datasets_data')}}
+
+), datasets_dataset AS (
+
+    SELECT 
+        user_id
+    FROM
+        {{ref('datasets_dataset')}}
+
+), final AS (
+    
+    SELECT 
+        IFNULL(dsd.user_id, dst.user_id) AS user_id,
+        dsd.aiblock_id,
+        dsd.account_id AS company_id,
+        COUNT(dsd.datapoint_id) AS Net_Data_Points, 
+        MAX(dsd.date_datapoint_uploaded) AS time_stamp
+    FROM datasets_data dsd
+    INNER JOIN datasets_dataset dst ON dsd.aiblock_id = dst.aiblock_id
+    GROUP BY 1, 2, 3 
+    ORDER BY 4 DESC;
+
+)
+
+
+SELECT 
+    *
+FROM final
+WHERE DATE(TIMESTAMP_TRUNC(time_stamp, DAY)) = DATE_SUB(CURRENT_DATE(),INTERVAL 1 DAY);
--- dbt/models/staging/segment_events/prediction_events.sql
+++ dbt/models/staging/segment_events/prediction_events.sql
@@ -1,1 +1,39 @@
-trial
+WITH prediction_models_classifier AS (
+
+    SELECT 
+        user_id,
+        aiblock_id,
+        classifier_id
+    FROM 
+        {{ref('prediction_models_classifier')}}
+
+), prediction_models_prediction AS (
+
+    SELECT
+        user_id,
+        account_id,
+        prediction_id,
+        date_prediction_made,
+        classifier_id
+    FROM 
+        {{ref('prediction_models_prediction')}}
+
+), final AS (
+
+    SELECT
+        IFNULL(pmp.user_id, pmc.user_id) AS user_id,
+        pmp.account_id AS company_id,
+        pmc.aiblock_id AS aiblock_id,
+        COUNT(pmp.prediction_id) AS total_predictions_24h,
+        MAX(pmp.date_prediction_made) AS time_stamp
+    FROM prediction_models_prediction pmp
+    INNER JOIN prediction_models_classifier pmc ON pmp.classifier_id = pmc.classifier_id
+    GROUP BY 1, 2, 3
+    ORDER BY 3 DESC;
+)
+
+
+SELECT
+    *
+FROM final
+WHERE DATE(TIMESTAMP_TRUNC(time_stamp, DAY)) = DATE_SUB(CURRENT_DATE(),INTERVAL 1 DAY);
