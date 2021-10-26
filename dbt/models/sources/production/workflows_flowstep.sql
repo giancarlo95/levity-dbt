7d7676a39a7a16a1920e556ae249fe301d3e0247
Giancarlo Masera <giancarlo@levity.ai>
2021-10-26 09:40:38 +0000

    use airbyte again + create new folder for website try

--- dbt/models/sources/production/workflows_flowstep.sql
+++ dbt/models/sources/production/workflows_flowstep.sql
@@ -1,6 +1,6 @@
 WITH source AS (
 
-    SELECT * FROM {{ source('google_cloud_postgresql_public', 'workflows_flowstep') }}
+    SELECT * FROM {{ source('public', 'production_workflows_flowstep') }}
 
 ),
 
@@ -9,13 +9,13 @@ renamed AS (
     SELECT
         id,			
         owner_id                                                      AS old_user_id,			
-        created_at                                                    AS date_step_created,			
+        CAST(created_at AS TIMESTAMP)                                 AS date_step_created,			
         updated_at                                                    AS date_step_updated,			
         workflow_id	                                                  AS flow_id,		
         frontegg_user_id                                              AS user_id,			
         frontegg_tenant_id                                            AS account_id,			
-        _fivetran_deleted,	
-        _fivetran_synced					
+        _airbyte_emitted_at,	
+        _airbyte_production_workflows_flowstep_hashid					
     
     FROM 
         source
