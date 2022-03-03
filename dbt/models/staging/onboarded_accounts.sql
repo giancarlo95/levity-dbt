51a8c187b5782c5a7a0673d3a7cc2535ccb70806
Giancarlo Masera <giancarlo@levity.ai>
2022-03-03 15:58:22 +0000

    commit

--- dbt/models/staging/onboarded_accounts.sql
+++ dbt/models/staging/onboarded_accounts.sql
@@ -12,9 +12,8 @@ SELECT
     -- to avoid remaking all the charts in the dashboard
     logged_account_id                      AS account_id,
     -- 
-    MIN(CASE WHEN is_deleted=1 THEN NULL ELSE user_email_address END) AS sample_user,
+    MIN(user_email_address)                AS sample_user,
     MIN(date_user_onboarded)               AS date_account_onboarded,
-    MAX(customer_status)                   AS customer_status,
     MAX(company_name)                      AS company_name
 FROM onboarded_users
 GROUP BY logged_account_id
