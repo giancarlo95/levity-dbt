8e476ae8261a972a0f13fd0fc1340ed28136a40b
Giancarlo Masera <giancarlo@levity.ai>
2022-05-12 15:52:21 +0000

    add workspace created date

--- dbt/models/staging/workspaces.sql
+++ dbt/models/staging/workspaces.sql
@@ -2,7 +2,8 @@ WITH legacy_workspaces AS (
 
     SELECT
        account_id                      AS workspace_id,
-       company_name                    AS workspace_name
+       company_name                    AS workspace_name,
+       date_account_onboarded          AS workspace_created_at
     FROM 
        {{ref('legacy_workspaces')}}
 
@@ -10,11 +11,23 @@ WITH legacy_workspaces AS (
 
     SELECT 
         id                    AS workspace_id,
-        name                  AS workspace_name
+        name                  AS workspace_name,
+        received_at           AS workspace_created_at
     FROM
         {{ref('django_production_workspaces')}}
 
+), unioned AS (
+
+    SELECT workspace_id, workspace_name FROM legacy_workspaces UNION DISTINCT
+    SELECT workspace_id, workspace_name FROM django_production_workspaces
+
 )
 
-SELECT * FROM legacy_workspaces UNION DISTINCT
-SELECT * FROM django_production_workspaces
+SELECT 
+    u.workspace_id,
+    u.workspace_name,
+    COALESCE(l.workspace_created_at, d.workspace_created_at) AS workspace_created_at
+FROM 
+    unioned u
+LEFT JOIN legacy_workspaces l USING(workspace_id)
+LEFT JOIN django_production_workspaces d USING(workspace_id)
