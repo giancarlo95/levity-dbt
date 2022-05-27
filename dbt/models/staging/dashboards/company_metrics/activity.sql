744922d9caf3ff8b5f317d5391b181f4c33edbdf
Giancarlo Masera <giancarlo@levity.ai>
2022-05-27 13:18:50 +0000

    activity dashboard

--- dbt/models/staging/dashboards/company_metrics/activity.sql
+++ dbt/models/staging/dashboards/company_metrics/activity.sql
@@ -20,7 +20,10 @@ WITH prediction_models_classifier AS (
         workspace_id,
         prediction_id,
         is_hitl,
-        origin,
+        CASE 
+            WHEN NOT(origin LIKE "flows%" OR origin LIKE "Integromat%" OR origin LIKE "Zapier%" OR origin = "test_tab" OR origin LIKE "Bubble%") THEN "API"
+            ELSE origin
+        END AS origin,
         workflow_id,
         date_prediction_made,
         classifier_id
@@ -47,39 +50,44 @@ WITH prediction_models_classifier AS (
     FROM
         {{ref('workspaces')}}
         
+), users AS (
+    
+    SELECT 
+        *
+    FROM
+        {{ref('users')}}
+ 
 ), final AS (
 
     SELECT
         pmp.workspace_id,
-        is_template,
-        is_hitl,
         origin,
-        workflow_id,
-        TIMESTAMP_TRUNC(pmp.date_prediction_made, DAY)        AS relevant_day,
-        COUNT(pmp.prediction_id)                              AS total_predictions,
-        MAX(date_prediction_made)                             AS time_stamp
+        EXTRACT(MONTH FROM pmp.date_prediction_made)          AS month,
+        EXTRACT(YEAR FROM pmp.date_prediction_made)           AS year,
+        COUNT(pmp.prediction_id)                              AS total_predictions
     FROM prediction_models_prediction pmp
     INNER JOIN prediction_models_classifier pmc ON pmp.classifier_id = pmc.classifier_id
     INNER JOIN datasets_dataset dd ON dd.aiblock_id=pmc.aiblock_id
-    WHERE TIMESTAMP_TRUNC(pmp.date_prediction_made, DAY) =  TIMESTAMP_TRUNC(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY), DAY)
+    INNER JOIN users u ON u.user_id=dd.user_id
+    WHERE 
+        NOT(origin = "test_tab")
+        AND pmp.date_prediction_made >= "2022-01-01"
     GROUP BY 
         1, 
         2, 
         3,
-        4,
-        5,
-        6
+        4
 
 )
 
 SELECT
-    final.workspace_id,
-    workspace_name AS workspace,
-    is_template,
-    is_hitl,
+    CONCAT(CAST(year AS STRING), CAST(month AS STRING)) AS year_month,
+    year,
+    month,
     origin,
-    workflow_id,
-    total_predictions,
-    time_stamp
+    workspace_name AS workspace,
+    total_predictions
 FROM final
 INNER JOIN workspaces oa ON final.workspace_id = oa.workspace_id
+ORDER BY
+    year_month ASC
