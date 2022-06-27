6d014dc477a156ebfb3f427d6ceca4ca18d455ed
Giancarlo Masera <giancarlo@levity.ai>
2022-06-27 12:02:26 +0000

    commit

--- dbt/models/staging/subscriptions/paymentplan.sql
+++ dbt/models/staging/subscriptions/paymentplan.sql
@@ -1,26 +1,23 @@
-WITH source AS (
+{{
+  config(
+    materialized = 'table',
+    )
+}}
 
-    SELECT * FROM {{ source('public', 'datasets_data') }}
+WITH accounts_paymentplan AS (
 
-),
-
-renamed AS (
-
-    SELECT
-        id                                   AS datapoint_id,
-        CAST(created_at AS TIMESTAMP)        AS date_datapoint_uploaded,
-        dataset_id                           AS aiblock_id, 
-        original_file_name,  
-        CAST(owner_id AS STRING)             AS old_user_id,
-        frontegg_user_id                     AS user_id,
-        frontegg_tenant_id                   AS workspace_id,
-        remote_url,  
-        storage_id,  
-        text, 
-        CAST(updated_at AS TIMESTAMP)        AS date_datapoint_updated
-    FROM source
+    SELECT 
+        *,
+        ROW_NUMBER() OVER(PARTITION BY frontegg_user_id, frontegg_tenant_id ORDER BY updated_at DESC) AS index
+    FROM
+        {{ref("accounts_paymentplan")}}
 
 )
 
-SELECT *
-FROM renamed
+SELECT 
+    *
+FROM
+    accounts_paymentplan
+WHERE 
+    index=1
+
