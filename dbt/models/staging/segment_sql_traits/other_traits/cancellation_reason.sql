5b65b5b2d4cf4ef31e94e23e3945849ec9809f7a
Giancarlo Masera <giancarlo@levity.ai>
2022-07-15 10:08:42 +0000

    commit

--- dbt/models/staging/segment_sql_traits/other_traits/cancellation_reason.sql
+++ dbt/models/staging/segment_sql_traits/other_traits/cancellation_reason.sql
@@ -14,21 +14,22 @@ WITH question_answered AS (
 
 SELECT
     user_id,
-    user_email_address,
+    text_answer,
+    --user_email_address,
     CASE
-        WHEN text_answer = "Not a good time" THEN "Time investment too high"
+        WHEN text_answer = "Timing problem" THEN "Time investment too high"
         WHEN text_answer = "Pricing" THEN "Pricing"
-        WHEN text_answer = "Just wanted to play around" THEN "Testing"
-        WHEN text_answer = "I need a security feature" THEN "Privacy/Security"
-        WHEN text_answer = "My team is not onboard" THEN "Not the Decision Maker"
-        WHEN text_answer = "No data/wrong templates" THEN "No Training Data"
-        WHEN text_answer = "Not a good fit for my use case" THEN "Use Case will never work"
-        WHEN text_answer = "I'm missing a crucial feature" THEN "On Hold for Product Feature"
-        WHEN text_answer = "It doesn't fit in my tech stack" THEN "Integration needs"  
+        WHEN text_answer = "Testing" THEN "Testing"
+        WHEN text_answer = "Privacy/Security" THEN "Privacy/Security"
+        WHEN text_answer = "Not the decision maker" THEN "Not the Decision Maker"
+        WHEN text_answer = "No raining data" THEN "No Training Data"
+        WHEN text_answer = "Will never work" THEN "Use Case will never work"
+        WHEN text_answer = "On Hold for the Product feature" THEN "On Hold for Product Feature"
+        WHEN text_answer = "Integration needs" THEN "Integration needs"  
         WHEN text_answer = "Other" THEN "Specific Reason (See Notes)"
     END AS hs_lead_status  
 FROM 
     question_answered
-INNER JOIN users USING(user_id)
+--INNER JOIN users USING(user_id)
 WHERE 
     question_name = "cancellation reason"
